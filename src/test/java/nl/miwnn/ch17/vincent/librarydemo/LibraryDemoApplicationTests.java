45bd41861a0105ce1f36bae6ee94bcd186d27a7e
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2025-10-27 14:46:59 +0100

    Initial commit

---
+++ src/test/java/nl/miwnn/ch17/vincent/librarydemo/LibraryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.ch17.vincent.librarydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
