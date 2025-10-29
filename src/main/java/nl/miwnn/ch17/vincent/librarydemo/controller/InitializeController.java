3ecba14eadc116f0891dc5abddf1ebfa582014b0
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2025-10-29 16:08:29 +0100

    Added authors and more books to the initializer

--- src/main/java/nl/miwnn/ch17/vincent/librarydemo/controller/InitializeController.java
+++ src/main/java/nl/miwnn/ch17/vincent/librarydemo/controller/InitializeController.java
@@ -1,12 +1,18 @@
 package nl.miwnn.ch17.vincent.librarydemo.controller;
 
+import nl.miwnn.ch17.vincent.librarydemo.model.Author;
 import nl.miwnn.ch17.vincent.librarydemo.model.Book;
 import nl.miwnn.ch17.vincent.librarydemo.model.Copy;
+import nl.miwnn.ch17.vincent.librarydemo.repositories.AuthorRepository;
 import nl.miwnn.ch17.vincent.librarydemo.repositories.BookRepository;
 import nl.miwnn.ch17.vincent.librarydemo.repositories.CopyRepository;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Controller;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Vincent Velthuizen
@@ -14,10 +20,12 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class InitializeController {
+    private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
     private final CopyRepository copyRepository;
 
-    public InitializeController(BookRepository bookRepository, CopyRepository copyRepository) {
+    public InitializeController(AuthorRepository authorRepository, BookRepository bookRepository, CopyRepository copyRepository) {
+        this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
         this.copyRepository = copyRepository;
     }
@@ -30,16 +38,38 @@ public class InitializeController {
     }
 
     private void initializeDB() {
-        makeBook("The Hobbit", 3);
-        makeBook("The Lord of the Rings", 5);
-        makeBook("The Name of the Wind", 2);
-        makeBook("A Wise Man's Fear", 1);
+        Author sanderson = makeAuthor("Brandon Sanderson");
+        Author rothfuss = makeAuthor("Patrick Rothfuss");
+        Author tolkien = makeAuthor("J.R.R. Tolkien");
+
+        makeBook("The Hobbit", 3, tolkien);
+        makeBook("The Lord of the Rings", 5, tolkien);
+        makeBook("The Name of the Wind", 2, rothfuss);
+        makeBook("A Wise Man's Fear", 1, rothfuss);
+        makeBook("Mistborn: The Final Empire", 3, sanderson);
+        makeBook("Mistborn: The Well of Ascension", 2, sanderson);
+        makeBook("Mistborn: The Hero of Ages", 2, sanderson);
+        makeBook("Fantasy: a collection", 2, tolkien, rothfuss, sanderson);
     }
 
-    private Book makeBook(String title, int numberOfCopies) {
+    private Author makeAuthor(String name) {
+        Author author = new Author();
+
+        author.setName(name);
+
+        authorRepository.save(author);
+
+        return author;
+    }
+
+    private Book makeBook(String title, int numberOfCopies, Author... authors) {
         Book book = new Book();
 
         book.setTitle(title);
+
+        Set<Author> authorSet = new HashSet<>(Arrays.asList(authors));
+        book.setAuthors(authorSet);
+
         bookRepository.save(book);
 
         for (int copyNr = 0; copyNr < numberOfCopies; copyNr++) {
